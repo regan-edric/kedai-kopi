90e0c2e58c1ee858fb86c16953a0e90a85855ea8
regan-edric <reganedric@gmail.com>
2024-02-24 15:19:50 +0700

    first commit

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#B6895B",
+        putihtulisan: "#FFF",
+      },
+      flex: {
+        about: "1 1 45rem",
+        contentabout: "1 1 35rem",
+      },
+      gridTemplateColumns: {
+        autofitmenu: "repeat(auto-fit, minmax(30rem, 1fr))",
+      },
+      zIndex: {
+        full: "9999",
+      },
+      backgroundColor: {
+        hitamTransparan: "rgba(0,0,0,0.5)",
+      },
+      margin: {
+        "15persen": "15%",
+      },
+    },
   },
   plugins: [],
-}
-
+};
